724f587c680b6fd4edec62dcc461b08ae1a83a9e
CyrilRaj333 <cyrilraj333@gmail.com>
2023-09-22 12:28:40 +0530

    task complted

---
+++ src/test/java/com/github/Hit.java
@@ -0,0 +1,20 @@
+package com.github;
+
+import org.testng.annotations.Test;
+
+public class Hit {
+	
+	@Test
+	
+      private void tc1() {
+	  System.out.println("tc1");
+      }
+	
+	  private void tc2() {
+	  System.out.println("tc2");
+      }
+	  
+      private void tc3() {
+	  System.out.println("tc3");
+      }
+}
